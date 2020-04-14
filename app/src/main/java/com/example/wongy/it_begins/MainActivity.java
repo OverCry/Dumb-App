5f4ae2580c557a37548d0eeb0e2ba243cf83c99b
OverCry <wongyeungchong@hotmail.com>
2020-04-14 19:42:35 +1200

    allow an interupt to scene transition

--- app/src/main/java/com/example/wongy/it_begins/MainActivity.java
+++ app/src/main/java/com/example/wongy/it_begins/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wongy.it_begins;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
@@ -11,7 +12,9 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 //    ImageView logo = findViewById(R.id.logo);
+    Boolean interrupt = false;
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,18 +23,25 @@ public class MainActivity extends AppCompatActivity {
         final ImageView logo = findViewById(R.id.logo);
         logo.setImageAlpha(250);
 
-        Handler transition = new Handler();
-        transition.postDelayed(new Runnable(){
 
+        Handler transition = new Handler();
+
+        //if nothing happens, go to main page
+        transition.postDelayed(new Runnable(){
             @Override
             public void run() {
-
+                if (!interrupt) {
+                    Intent toRemainders = new Intent(MainActivity.this, ReminderCollect.class);
+                    MainActivity.this.startActivity(toRemainders);
+                }
             }
-        }, 500);
+        }, interrupt, 4000);
 
+        //if icon is clicked before change, show Special activity
         findViewById(R.id.logo).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                logo.setImageAlpha(100);
                 Handler handler = new Handler();
                 handler.postDelayed(new newRunnable(), 250);
             }
@@ -40,8 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
                  @Override
                  public void run() {
-                     int alpha = logo.getImageAlpha();
-                     logo.setImageAlpha(alpha - 50);
+                     logo.setImageAlpha(0);
+                     interrupt = true;
+                     Intent yay = new Intent(MainActivity.this, Special.class);
+                     MainActivity.this.startActivity(yay);
                  }
              }
 
---
+++ app/src/main/java/com/example/wongy/it_begins/Special.java
@@ -0,0 +1,13 @@
+package com.example.wongy.it_begins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Special extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_special);
+    }
+}
