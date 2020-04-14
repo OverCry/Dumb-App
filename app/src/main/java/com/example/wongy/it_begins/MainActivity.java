5006fa4151ebf48022f35f621481bfa9796a37b9
OverCry <wongyeungchong@hotmail.com>
2020-04-14 19:22:58 +1200

    Added new activity to transit across

--- app/src/main/java/com/example/wongy/it_begins/MainActivity.java
+++ app/src/main/java/com/example/wongy/it_begins/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.wongy.it_begins;
 
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+//    ImageView logo = findViewById(R.id.logo);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView logo = findViewById(R.id.logo);
+        logo.setImageAlpha(250);
+
+        Handler transition = new Handler();
+        transition.postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+
+            }
+        }, 500);
+
+        findViewById(R.id.logo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Handler handler = new Handler();
+                handler.postDelayed(new newRunnable(), 250);
+            }
+
+             class newRunnable implements Runnable{
+
+                 @Override
+                 public void run() {
+                     int alpha = logo.getImageAlpha();
+                     logo.setImageAlpha(alpha - 50);
+                 }
+             }
+
+        });
+
     }
+
+
 }
