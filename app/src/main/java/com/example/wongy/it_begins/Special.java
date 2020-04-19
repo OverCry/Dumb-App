b7594e017867708e76201cb89cb88913fed568b1
OverCry <wongyeungchong@hotmail.com>
2020-04-19 16:21:50 +1200

    Disables/enables button

--- app/src/main/java/com/example/wongy/it_begins/Special.java
+++ app/src/main/java/com/example/wongy/it_begins/Special.java
@@ -1,8 +1,15 @@
 package com.example.wongy.it_begins;
 
+import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,14 +22,28 @@ public class Special extends AppCompatActivity {
 
         final Button lolicon = findViewById(R.id.youAreLolicon);
 
+
         lolicon.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
             @Override
             public void onClick(View view) {
-
+                //disable rapid executions
+                lolicon.setEnabled(false);
                 MediaPlayer play = MediaPlayer.create(Special.this,R.raw.youarelolicon);
                 play.start();
+
+                //enable button press
+                Handler enabler = new Handler();
+                enabler.postDelayed(new Runnable(){
+                    @Override
+                    public void run() {
+                        lolicon.setEnabled(true);
+                    }
+                }, 9000);
+
             }
         });
+
 
     }
 }
