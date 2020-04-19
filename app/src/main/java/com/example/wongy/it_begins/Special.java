a9710a3811b09fc5cce34b4a133728dbb7f671e2
OverCry <wongyeungchong@hotmail.com>
2020-04-19 16:09:28 +1200

    adding lolicon button???

--- app/src/main/java/com/example/wongy/it_begins/Special.java
+++ app/src/main/java/com/example/wongy/it_begins/Special.java
@@ -1,7 +1,10 @@
 package com.example.wongy.it_begins;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Special extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class Special extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_special);
+
+        final Button lolicon = findViewById(R.id.youAreLolicon);
+
+        lolicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MediaPlayer play = MediaPlayer.create(Special.this,R.raw.youarelolicon);
+                play.start();
+            }
+        });
+
     }
 }
